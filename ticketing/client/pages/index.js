ea620d52650ea16b3892c99b1a121766935df6f0
Rahul Nallappa <rahul.nallappa@gmail.com>
2020-12-06 18:28:12 -0600

    revised comments to common module

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,6 +1,6 @@
 import buildClient from '../api/build-client';
 // when nextjs boots up, it searches for pages directory
-// nextjs interprets file names within pages directory and map them to file paths in browser
+// nextjs interprets file names within pages directory and maps them to file paths in browser
 
 const LandingPage = ({ currentUser }) => {
   return <h1>You are {currentUser ? 'signed in' : 'not signed in'}</h1>;
